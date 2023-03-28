fc4449044b2cef0602194b3d19c26666f097fc41
Elizabeth Thorner <73670368+ElizabethThorner@users.noreply.github.com>
2023-03-28 14:11:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <vector>
 
+void Task1 (std::vector<std::vector<int>>& stocks) {
+
+}
+
+void Task2(std::vector<std::vector<int>>& stocks) {
+
+}
+
+void Task3a(std::vector<std::vector<int>>& stocks) {
+
+}
+
+void Task3b(std::vector<std::vector<int>>& stocks) {
+
+}
+
+void Task4(std::vector<std::vector<int>>& stocks, int k) {
+
+}
+
+void Task5(std::vector<std::vector<int>>& stocks, int k) {
+
+}
+
+void Task6(std::vector<std::vector<int>>& stocks, int k) {
+
+}
+
 int main() {
-    std::cout << "Input the task to be completed:" << std::endl;
     std::string taskInput;
     std::getline(std::cin, taskInput);
     int k = 0;
@@ -30,7 +57,20 @@ int main() {
         }
         stocks[i] = column;
     }
+    if (taskInput == "Stocks 1") {
+        Task1(stocks);
+    } else if (taskInput == "Stocks 2") {
+        Task2(stocks);
+    } else if (taskInput == "Stocks 3a") {
+        Task3a(stocks);
+    } else if (taskInput == "Stocks 3b") {
+        Task3b(stocks);
+    } else if (taskInput == "Stocks 4") {
+        Task4(stocks, k);
+    } else if (taskInput == "Stocks 5") {
+        Task5(stocks, k);
+    } else {
+        Task6(stocks, k);
+    }
     return 0;
 }
-
-
