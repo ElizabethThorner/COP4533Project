b08a6d697698229a963281e47e44ba349f4f99e2
Elizabeth Thorner <73670368+ElizabethThorner@users.noreply.github.com>
2023-03-28 14:26:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,22 +31,22 @@ void Task6(std::vector<std::vector<int>>& stocks, int k) {
 
 int main() {
     std::string taskInput;
-    std::getline(std::cin, taskInput);
+    std::getline(std::cin, taskInput);  //This takes in the argument indicating the task to be executed
     int k = 0;
-    if ((taskInput == "Stocks 4") || (taskInput == "Stocks 5") || (taskInput == "Stocks 6")) {
-        std::cin >> k;
+    if ((taskInput == "Stocks 4") || (taskInput == "Stocks 5") || (taskInput == "Stocks 6")) { //Because k is only used for Problem 2
+        std::cin >> k; //This takes in line 1 of the input for Problem 2
     }
     std::string input;
-    std::getline(std::cin, input);
-    int m = std::stoi(input.substr(0, input.find(' ')));
-    std::vector<std::vector<int>> stocks(m);
-    int n = std::stoi(input.substr(input.find(' ') + 1));
-    for (int i = 0; i < m; i++) {
-        std::vector<int> column(n);
-        std::getline(std::cin, input);
+    std::getline(std::cin, input); //This takes in line 1 for the input for Problem 1 or line 2 of the input for Problem 2
+    int m = std::stoi(input.substr(0, input.find(' '))); //This parses the input to find m
+    std::vector<std::vector<int>> stocks(m); //This initializes the matrix
+    int n = std::stoi(input.substr(input.find(' ') + 1)); //This parses the input to find n
+    for (int i = 0; i < m; i++) { //This goes through the following m lines of input
+        std::vector<int> column(n); //This initializes the column to be added to the matrix
+        std::getline(std::cin, input); //This takes in the input
         std::string integer;
         int columnIndex = 0;
-        for (int j = 0; j < input.length(); j++) {
+        for (int j = 0; j < input.length(); j++) { //This loop parses the input to add each integer to the column
             if ((input[j] == ' ') || (j == input.length() - 1)) {
                 column[columnIndex] = std::stoi(integer);
                 integer = "";
@@ -55,7 +55,7 @@ int main() {
                 integer += input[j];
             }
         }
-        stocks[i] = column;
+        stocks[i] = column; //The column is added to the matrix
     }
     if (taskInput == "Stocks 1") {
         Task1(stocks);
@@ -74,3 +74,4 @@ int main() {
     }
     return 0;
 }
+
